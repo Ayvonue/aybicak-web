f96eeba76a09b0380223ea3f12bacb7c4b6aecfc
Ayvonue <ayvonue@users.noreply.github.com>
2026-01-31 12:31:08 +0300

    Add dark background behind logo for better integration

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -80,7 +80,7 @@ export default function Navbar() {
 
                         {/* Center: Logo */}
                         <Link href="/" className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60]">
-                            <div className="relative w-56 h-14 md:w-80 md:h-20 transition-transform hover:scale-105 flex items-center justify-center">
+                            <div className="relative h-12 md:h-16 px-4 py-2 transition-transform hover:scale-105 flex items-center justify-center bg-gradient-to-r from-slate-800/90 to-slate-900/90 rounded-xl border border-white/10">
                                 {/* eslint-disable-next-line @next/next/no-img-element */}
                                 <img
                                     src="/logo-transparent.png"
